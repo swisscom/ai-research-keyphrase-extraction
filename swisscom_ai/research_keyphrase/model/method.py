731ac866e0efd6383889bcf5835501a307d47c2e
Kamil <kamil.bennani-smires@epfl.ch>
2017-09-22 13:57:30 +0200

    Enforce N to be at most the number of the candidates

--- swisscom_ai/research_keyphrase/model/method.py
+++ swisscom_ai/research_keyphrase/model/method.py
@@ -24,49 +24,46 @@ def _MMR(embdistrib, text_obj, candidates, X, beta, N, use_filtered):
     3)list containing for each keyphrase a list of alias (list of list of string)
     """
 
-    if len(candidates) >= N:  # If #candidates >= N result will be sorted
+    N = min(N, len(candidates))
+    doc_embedd = extract_doc_embedding(embdistrib, text_obj, use_filtered)  # Extract doc embedding
+    doc_sim = cosine_similarity(X, doc_embedd.reshape(1, -1))
 
-        doc_embedd = extract_doc_embedding(embdistrib, text_obj, use_filtered)  # Extract doc embedding
-        doc_sim = cosine_similarity(X, doc_embedd.reshape(1, -1))
+    doc_sim_norm = doc_sim/np.max(doc_sim)
+    doc_sim_norm = 0.5 + (doc_sim_norm - np.average(doc_sim_norm)) / np.std(doc_sim_norm)
 
-        doc_sim_norm = doc_sim/np.max(doc_sim)
-        doc_sim_norm = 0.5 + (doc_sim_norm - np.average(doc_sim_norm)) / np.std(doc_sim_norm)
+    sim_between = cosine_similarity(X)
+    np.fill_diagonal(sim_between, np.NaN)
 
-        sim_between = cosine_similarity(X)
-        np.fill_diagonal(sim_between, np.NaN)
+    sim_between_norm = sim_between/np.nanmax(sim_between, axis=0)
+    sim_between_norm = \
+        0.5 + (sim_between_norm - np.nanmean(sim_between_norm, axis=0)) / np.nanstd(sim_between_norm, axis=0)
 
-        sim_between_norm = sim_between/np.nanmax(sim_between, axis=0)
-        sim_between_norm = \
-            0.5 + (sim_between_norm - np.nanmean(sim_between_norm, axis=0)) / np.nanstd(sim_between_norm, axis=0)
+    selected_candidates = []
+    unselected_candidates = [c for c in range(len(candidates))]
 
-        selected_candidates = []
-        unselected_candidates = [c for c in range(len(candidates))]
+    j = np.argmax(doc_sim)
+    selected_candidates.append(j)
+    unselected_candidates.remove(j)
 
-        j = np.argmax(doc_sim)
-        selected_candidates.append(j)
-        unselected_candidates.remove(j)
+    for _ in range(N - 1):
+        selec_array = np.array(selected_candidates)
+        unselec_array = np.array(unselected_candidates)
 
-        for _ in range(N - 1):
-            selec_array = np.array(selected_candidates)
-            unselec_array = np.array(unselected_candidates)
+        distance_to_doc = doc_sim_norm[unselec_array, :]
+        dist_between = sim_between_norm[unselec_array][:, selec_array]
+        if dist_between.ndim == 1:
+            dist_between = dist_between[:, np.newaxis]
+        j = np.argmax(beta * distance_to_doc - (1 - beta) * np.max(dist_between, axis=1).reshape(-1, 1))
+        item_idx = unselected_candidates[j]
+        selected_candidates.append(item_idx)
+        unselected_candidates.remove(item_idx)
 
-            distance_to_doc = doc_sim_norm[unselec_array, :]
-            dist_between = sim_between_norm[unselec_array][:, selec_array]
-            if dist_between.ndim == 1:
-                dist_between = dist_between[:, np.newaxis]
-            j = np.argmax(beta * distance_to_doc - (1 - beta) * np.max(dist_between, axis=1).reshape(-1, 1))
-            item_idx = unselected_candidates[j]
-            selected_candidates.append(item_idx)
-            unselected_candidates.remove(item_idx)
+    # Not using normalized version of doc_sim for computing relevance
+    relevance_list = max_normalization(doc_sim[selected_candidates]).tolist()
+    aliases_list = get_aliases(sim_between[selected_candidates, :], candidates)
 
-        # Not using normalized version of doc_sim for computing relevance
-        relevance_list = max_normalization(doc_sim[selected_candidates]).tolist()
-        aliases_list = get_aliases(sim_between[selected_candidates, :], candidates)
+    return candidates[selected_candidates].tolist(), relevance_list, aliases_list
 
-        return candidates[selected_candidates].tolist(), relevance_list, aliases_list
-    else:
-        # If candidates < N return all the candidates (not sorted)
-        return candidates.tolist()
 
 
 def MMRPhrase(embdistrib, text_obj, beta=0.5, N=10, use_filtered=True):
