354e7b3be63f7e2fffc23b334b3ec50514545ff7
Komlan Gluck Charles Bakar <KomlanGluckCharles.Bakar@softwareag.com>
2021-12-05 12:58:16 +0100

    load doc2vec and sbert

--- launch.py
+++ launch.py
@@ -1,14 +1,28 @@
 import argparse
 from configparser import ConfigParser
 
-from swisscom_ai.research_keyphrase.embeddings.emb_distrib_local import EmbeddingDistributorLocal
+from swisscom_ai.research_keyphrase.embeddings.emb_distrib_local import (
+    EmbeddingDistributorLocal,
+)
+
+from swisscom_ai.research_keyphrase.embeddings.emb_distrib_doc2vec import (
+    EmbeddingDistributorDoc2Vec,
+)
+
+from swisscom_ai.research_keyphrase.embeddings.emb_distrib_sentencetransformers import (
+    EmbeddingDistributorSBert,
+)
+
+
 from swisscom_ai.research_keyphrase.model.input_representation import InputTextObj
 from swisscom_ai.research_keyphrase.model.method import MMRPhrase
 from swisscom_ai.research_keyphrase.preprocessing.postagging import PosTaggingCoreNLP
 from swisscom_ai.research_keyphrase.util.fileIO import read_file
 
 
-def extract_keyphrases(embedding_distrib, ptagger, raw_text, N, lang, beta=0.55, alias_threshold=0.7):
+def extract_keyphrases(
+    embedding_distrib, ptagger, raw_text, N, lang, beta=0.55, alias_threshold=0.7
+):
     """
     Method that extract a set of keyphrases
 
@@ -26,35 +40,49 @@ def extract_keyphrases(embedding_distrib, ptagger, raw_text, N, lang, beta=0.55,
     """
     tagged = ptagger.pos_tag_raw_text(raw_text)
     text_obj = InputTextObj(tagged, lang)
-    return MMRPhrase(embedding_distrib, text_obj, N=N, beta=beta, alias_threshold=alias_threshold)
+    return MMRPhrase(
+        embedding_distrib, text_obj, N=N, beta=beta, alias_threshold=alias_threshold
+    )
 
 
 def load_local_embedding_distributor():
     config_parser = ConfigParser()
-    config_parser.read('config.ini')
-    sent2vec_model_path = config_parser.get('SENT2VEC', 'model_path')
+    config_parser.read("config.ini")
+    sent2vec_model_path = config_parser.get("SENT2VEC", "model_path")
     return EmbeddingDistributorLocal(sent2vec_model_path)
+
+
+def load_doc2vec_embedding_distributor():
+    config_parser = ConfigParser()
+    config_parser.read("config.ini")
+    sent2vec_model_path = config_parser.get("DOC2VEC", "model_path")
+    return EmbeddingDistributorDoc2Vec(sent2vec_model_path)
+
+
+def load_sbert_embedding_distributor():
+    return EmbeddingDistributorSBert()
 
 
 def load_local_corenlp_pos_tagger():
     config_parser = ConfigParser()
-    config_parser.read('config.ini')
-    host = config_parser.get('STANFORDCORENLPTAGGER', 'host')
-    port = config_parser.get('STANFORDCORENLPTAGGER', 'port')
+    config_parser.read("config.ini")
+    host = config_parser.get("STANFORDCORENLPTAGGER", "host")
+    port = config_parser.get("STANFORDCORENLPTAGGER", "port")
     return PosTaggingCoreNLP(host, port)
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Extract keyphrases from raw text')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Extract keyphrases from raw text")
 
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('-raw_text', help='raw text to process')
-    group.add_argument('-text_file', help='file containing the raw text to process')
-    
+    group.add_argument("-raw_text", help="raw text to process")
+    group.add_argument("-text_file", help="file containing the raw text to process")
 
-    parser.add_argument('-tagger_host', help='CoreNLP host', default='localhost')
-    parser.add_argument('-tagger_port', help='CoreNLP port', default=9000)
-    parser.add_argument('-N', help='number of keyphrases to extract', required=True, type=int)
+    parser.add_argument("-tagger_host", help="CoreNLP host", default="localhost")
+    parser.add_argument("-tagger_port", help="CoreNLP port", default=9000)
+    parser.add_argument(
+        "-N", help="number of keyphrases to extract", required=True, type=int
+    )
     args = parser.parse_args()
 
     if args.text_file:
@@ -64,4 +92,4 @@ if __name__ == '__main__':
 
     embedding_distributor = load_local_embedding_distributor()
     pos_tagger = load_local_corenlp_pos_tagger(args.tagger_host, args.tagger_port)
-    print(extract_keyphrases(embedding_distributor, pos_tagger, raw_text, args.N, 'en'))
+    print(extract_keyphrases(embedding_distributor, pos_tagger, raw_text, args.N, "en"))
